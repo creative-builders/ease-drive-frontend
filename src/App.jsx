d1c89fca503285e1ee4dfb9311d99e2ad85316c3
SoloBaChi <solobachi02@gmail.com>
2025-01-02 01:48:50 +0100

    Setup Packages

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center uppercase border border-red-900 p-4'>Welcome to Ease Drive</div>
+  )
+}
+
+export default App
