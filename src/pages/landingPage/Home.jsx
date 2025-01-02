1ff0f1ca922358612f216d140634f84c81f81959
SoloBaChi <solobachi02@gmail.com>
2025-01-02 02:02:53 +0100

    added other setup folders

---
+++ src/pages/landingPage/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-center uppercase border border-red-900 p-4'>Welcome to Ease Drive</div>
+  )
+}
+
+export default Home
