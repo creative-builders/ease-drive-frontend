c14b33e14e2fa9a270da0d33d4b37b1a5e61442f
SoloBaChi <solobachi02@gmail.com>
2025-01-06 18:21:30 +0100

    Added footer component

--- src/pages/landingPage/Home.jsx
+++ src/pages/landingPage/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Footer from './Footer'
 
 const Home = () => {
   return (
     <div className='text-center uppercase border border-red-900 p-4'>
       Welcome to Ease Drive
       <p>this is how we started</p>
+
+      <Footer/>
       </div>
   )
 }
