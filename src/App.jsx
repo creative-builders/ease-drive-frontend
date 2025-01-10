1af9f894d658987246cbf2f9b2ae8a8664bfd95c
De-natural <festuschidubem376@gmail.com>
2025-01-10 11:40:28 -0800

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Home from './pages/landingPage/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/landingPage/Home';
+import DriverLogin from './pages/auth/DriverLogin/Driverlogin';
+import DriverSign from './pages/auth/DriverSign/DriverSign';
 
 const App = () => {
   return (
-    <Home/>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/DriverLogin" element={<DriverLogin />} />
+        <Route path="/DriverSign" element={<DriverSign />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/pages/landingPage/Home.jsx
+++ src/pages/landingPage/Home.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import Footer from './Footer'
+import React, { useState } from 'react';
+import Footer from './Footer';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const [active, setActive] = useState(false);
+  const navigate = useNavigate();
+
+  const handleDriversignup = () => {
+    navigate('/DriverSign');
+  };
+
+  const handleDriverlogin = () => {
+    navigate('/DriverLogin');
+  };
+
   return (
     <div className='text-center uppercase border border-red-900 p-4'>
       Welcome to Ease Drive
       <p>this is how we started</p>
 
-      <Footer/>
-      </div>
-  )
-}
+      <Footer />
+      <main className='h-16 w-[50%] mx-10 flex border border-red-600 items-center justify-around'>
+        <button
+          className='h-10 w-20 text-white bg-blue-500'
+          onClick={handleDriversignup}
+        >
+          SignUp
+        </button>
+        <button
+          className='h-10 w-20 text-white bg-green-500'
+          onClick={handleDriverlogin}
+        >
+          LogIn
+        </button>
+      </main>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
