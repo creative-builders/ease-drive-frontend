c738ab41bf4c60d99ea401241d75b5ef913aed5f
SoloBaChi <solobachi02@gmail.com>
2025-01-19 00:52:54 +0100

    Minor Update

--- src/pages/landingPage/Home.jsx
+++ src/pages/landingPage/Home.jsx
@@ -3,9 +3,10 @@ import Footer from './Footer'
 
 const Home = () => {
   return (
-    <div className='text-center uppercase border border-red-900 p-4'>
+    <div className='p-0 m-0  text-center uppercase border border-red-900 p-4'>
       Welcome to Ease Drive
       <p>this is how we started</p>
+      <span className='p-0 m-0 m-0 p-0 m-0'></span>
 
       <Footer/>
       </div>
