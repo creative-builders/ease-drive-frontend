aad772c1b6dc0e043454a20ddb25ad1ca7fb6375
SoloBaChi <solobachi02@gmail.com>
2025-01-30 20:19:18 +0100

    Done with the setup files

--- src/pages/auth/SignupOptions/Options.jsx
+++ src/pages/auth/SignupOptions/Options.jsx
@@ -1,7 +1,9 @@
 
 import { useState } from 'react';
 import MobilePic from '/Rectangle.png'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import leftCarImage from "../../../assets/images/signup-options-left-image.svg"
+import { BiChevronRight } from 'react-icons/bi';
 
 export default function Option() {
   const navigate =  useNavigate();
@@ -15,72 +17,76 @@ export default function Option() {
 
   return (
     
-    <div className="h-screen w-full flex flex-col items-center justify-evenly relative">
-        
-        <nav className="h-17 w-full flex items-center justify-around absolute top-0 border-b-2">
-          <p className='text-white text-2xl z-30 font-semibold'>EaseDrive</p>
-          <ul className="z-10 text-white w:2/5 md:h-14 w-96 flex items-center justify-evenly">
-            <p className='text-wrap text-[12px] md:text-sm'>Already have an account</p>
-            <button className="border border-green-900 h-10 w-24 cursor-pointer rounded-xl text-green-600">
-              Login
-            </button>
-          </ul>
-        </nav>
+    <div className="h-screen lg:flex lg:flex-row lg:gap-x-[45px] lg:items-center">
+      <div 
+      style={{background:`linear-gradient(0deg, rgba(43, 170, 66, 0.44) 0%, rgba(43, 170, 66, 0.44) 100%), url(${leftCarImage}) lightgray bottom left / cover no-repeat`}}
+      className="mb-6 basis-1/2 h-[421px] lg:h-screen border border-red-900">
+        {/* <img  alt="" /> */}
+      </div>
 
-       
-        <div className="box h-full w-full flex items-center justify-between">
-          
-          <div className="carPicture h-[70vh] md:h-full w-1/2 bg-red-300 relative">
-            <img className=" w-full h-full object-cover object-bottom absolute" src={MobilePic} alt="" />
-            <div className='relative w-full h-full z-50 flex items-end justify-center pb-20'>
-              <p className='text-wrap text-white md:text-white text-2xl md:text-4xl font-bold'>Escape the usual <br className='hidden md:block'/> struggle of public buses.</p>
-            </div>
-          </div>
+        <div className="lg:basis-1/2 pb-[40px] px-[4%]">
+            <div className="hidden lg:flex justify-center items-center gap-x-[42px]">
+                    <p>Already have have an account ?</p>
+                    <Link to="/passenger-login" className="text-green-300 border border-green-300 px-6 py-3 rounded-lg">Login</Link>
+                </div>
 
-          
-          <div className="form h-full w-7">
-            <main className="sly h-106 w-91 mx-auto mt-36 px-5 py-3 flex flex-col gap-5 items-center justify-around">
-              <p>Sign up as a</p>
-
+              <h1 className='font-bold mb-8 text-gray-900 text-xl lg:text-2xl'>Sign up as a</h1>
               {/* Passenger Section */}
-              <section
-                className={`h-24 w-4/5 relative border px-2 py-3 flex flex-col gap-2 rounded-lg cursor-pointer ${
-                  selectedOption === 'passenger-login'
-                    ? 'border-purple-900'
+              <div
+                className={`mb-8 min-h-[93px] border p-4 rounded-[20px] cursor-pointer ${
+                  selectedOption === '/passenger-login'
+                    ? 'border-green-300'
                     : 'border-gray-300'
                 }`}
                 onClick={() => handleSelect('/passenger-login')}
               >
-                <h2 className="font-semibold">Passenger</h2>
-                <p>Effortlessly book your trip with Easedrive</p>
-                <i className="pin absolute text-base right-1 top-8 fa fa-angle-right" aria-hidden="true"></i>
-              </section>
+               <div className="flex items-center">
+                <div className='basis-[calc(100%-54px)]'>
+                <h2 className="font-black mb-2 text-gray-900 text-base lg:text-xl">Passenger</h2>
+                <p className='text-gray-400 text-xs lg:text-base'>Effortlessly book your trips with EaseDrive and easily manage all your bookings in one convenient place</p>
+                </div>
+                <span className='inline-block ml-auto'>
+                  <BiChevronRight fontSize={"24px"}/>
+                </span>
+               </div>
+                {/* <i className="pin absolute text-base right-1 top-8 fa fa-angle-right" aria-hidden="true"></i> */}
+              </div>
 
-              <section
-                className={`h-28 w-4/5 relative border px-2 py-2 flex flex-col gap-2 rounded-lg cursor-pointer text-sm ${
-                  selectedOption === 'vehicleOwner'
-                    ? 'border-purple-900'
+              <div
+                className={`mb-8 min-h-[93px] border p-4 rounded-[20px] cursor-pointer text-sm ${
+                  selectedOption === '/driver-signup'
+                    ? 'border-green-300'
                     : 'border-gray-300'
                 }`}
                 onClick={() => handleSelect('/driver-signup')}
               >
-                <h2 className="font-semibold">Vehicle Owners</h2>
-                <p>
+               <div className="flex items-center">
+                <div className='basis-[calc(100%-54px)]'>
+                <h2 className="font-black mb-2 text-gray-900 text-base lg:text-xl">Vehicle Owners</h2>
+                <p className='text-gray-400 text-xs lg:text-base'>
                   Join us as a Vehicle owner and start earning income from your
                   Vehicle when they are in active use for trip
                 </p>
-                <i className="pin absolute text-base right-1 top-10 fa fa-angle-right" aria-hidden="true"></i>
-              </section>
+                </div>
+                <span className='inline-block ml-auto'>
+                  <BiChevronRight fontSize={"24px"}/>
+                </span>
+               </div>
+                {/* <i className="pin absolute text-base right-1 top-10 fa fa-angle-right" aria-hidden="true"></i> */}
+              </div>
 
               <button
                onClick={() => navigate(selectedOption)}
-                className="h-12 w-4/5 rounded-md bg-green-600 text-white disabled:bg-gray-400"
+                className="mb-[42px] px-3 py-4 w-full rounded-md bg-green-600 text-white disabled:bg-green-100"
                 disabled={!selectedOption}
               >
                 Continue
               </button>
-            </main>
-          </div>
+
+               <div className="lg:hidden flex justify-center gap-x-2">
+                    <p>Already have have an account ?</p>
+                    <Link to="/passenger-login" className="text-green-300">Login</Link>
+                </div>
         </div>
       </div>
   );
