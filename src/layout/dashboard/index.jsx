65b61e84f6ca90d0405c5bb558d2f35e3b1c69ef
Solomon Chiakonam <solobachi02@gmail.com>
2025-09-12 14:32:35 +0100

    added fetching user details on loggined

--- src/layout/dashboard/index.jsx
+++ src/layout/dashboard/index.jsx
@@ -2,12 +2,44 @@ import { Outlet } from "react-router-dom"
 import {Navbar} from "../../components/dashboard/layout/Navbar"
 import { useState } from "react";
 import { SideBarMenu } from "../../components/dashboard/layout/SideBarMenu";
+import { useQuery } from "@tanstack/react-query";
+import { getUserProfile } from "../../store/users/api";
+import { userAtom } from "../../components/atoms/userAtom";
+import useAuth from "../../hooks/useAuth";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import toast from "react-hot-toast";
 
 
 
 export const DashboardLayout = () => {
     const [coords, setCoords] = useState(null);
-      const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const setUser = useSetRecoilState(userAtom); 
+    const logginedUser = useRecoilValue(userAtom);
+
+    const { isLoading } = useQuery(["getUserProfile", logginedUser?.id || logginedUser?._id], getUserProfile,
+      {
+        onSuccess: (response) => {
+          setUser(response?.data)
+        },
+        
+        onError:(error) => {
+        toast.error(error?.message || error?.response?.data?.message)
+        // If authentication fails (no token or invalid token), redirect to login page
+        navigate('/'); 
+        }
+      }
+    );
+
+    if(isLoading){
+      return (
+      <div className="flex justify-center items-center">
+        <p className="text-blue-500 text-base font-medium">Loading User Dashboard</p>
+      </div>
+      )
+    }
+
+
   return (
      <>
      <Navbar
@@ -22,12 +54,6 @@ export const DashboardLayout = () => {
           </div>
         </div>
      </div>
-     {/* <BackgroundMap coords={coords} /> */}
-     
-       {/* Page content */}
-      {/* <div className="relative z-10 pt-16 overflow-auto xl:pb-[74px]">
-        <Outlet context={{ coords, setCoords}} />
-      </div> */}
      </>
   )
 }
