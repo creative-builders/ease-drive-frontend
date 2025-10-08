58a5d2f7e9b7261ab41af5bda7a713af3777ad10
gionsunday <richardadaji@gmail.com>
2025-10-08 17:23:52 +0100

    y

--- src/components/BackArrow.jsx
+++ src/components/BackArrow.jsx
@@ -1,5 +1,6 @@
 
 import arrow from '/src/assets/images/arrow-left.png'
+import { FaArrowLeft } from "react-icons/fa"
 import { useNavigate } from 'react-router-dom'
 
 const BackArrow = ({ extendedStyles }) => {
@@ -17,8 +18,13 @@ const BackArrow = ({ extendedStyles }) => {
   return (
     <div className={` inline-flex items-center gap-2 z-0 md:z-10 absolute left-6 cursor-pointer ${extendedStyles}`} onClick={handleGoBack}>
       {/* <img  src={arrow} alt="" /> */}
+      <div className='flex gap-4 px-2 py-3'>
+        <button onClick={handleGoBack} className='flex font-regular text-xl'>
+          <FaArrowLeft />
+        </button>
+      </div>
       <div className='h-6 w-6'>
-        <BackArrow />
+
       </div>
       {/* <span className='text-xs not-italic font-normal leading-normal'>back</span> */}
     </div>
