0e622bc3d66e0efba69ebafb09ad39ce2ac1003a
Solomon Chiakonam <solobachi02@gmail.com>
2025-11-23 19:00:57 +0100

    refactored home dashboard loader

--- src/components/dashboard/loaders/SideBarMenuLoader.jsx
+++ src/components/dashboard/loaders/SideBarMenuLoader.jsx
@@ -1,14 +1,8 @@
+import { ShimmerBlock } from "./DashboardHomeLoader";
 
 
 export const SideBarMenuLoader = () => {
   return (
-    <div
-      className="relative mb-4 w-full rounded-lg36 px-[14px] py-2 h-[56px] overflow-hidden bg-white rounded-lg p-4"
-      aria-hidden="true"
-    >
-      {/* Shimmer effect */}
-      <div className="absolute inset-0 -translate-x-full animate-shimmer bg-gradient-to-r from-transparent via-neutral-50 to-neutral-50" />
-    </div>
-
+    <ShimmerBlock extendedStyles={"mb-4 w-full rounded-lg36 px-[14px] py-2 h-[56px] rounded-lg p-4"}/>
   );
 };
--- src/pages/dashboard/passenger/Rides.jsx
+++ src/pages/dashboard/passenger/Rides.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
 
 
 export const Rides = () => {
+  // const tabs = ["Ride History", "Ongoing Rides", "Scheduled Rides"];
+  const tabs = ["Ride History", "Ongoing Rides"];
+
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
   return (
-    <div className="text-center">Rides</div>
+    <div className="ml-4 lg:ml-0">
+      <div className="flex items-center gap-x-2.5">
+        <h2 className="basis-[178px] text-lg font-bold text-gray-950">Your Rides</h2>
+        <div>
+          {/* Tabs */}
+          <div className="flex gap-x-1.5 ml-auto justify-center items-center bg-white rounded-[10px] h-[51px] border-b border-gray-300">
+            {tabs.map((tab) => (
+              <button
+                onClick={() => setActiveTab(tab)}
+                key={tab}
+                className={`block w-[166px] h-full lg:w-[241px] px-[5px] py-2 rounded-[10px] lg:px-4 text-gray-950 font-medium ${activeTab === tab ? 'bg-primary-700 text-white' : ''}`}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+          {/* Tab Content */}
+          <div>
+            {/* Content for the selected tab goes here */}
+          </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
