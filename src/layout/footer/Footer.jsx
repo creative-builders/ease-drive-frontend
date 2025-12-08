ddb1566f0b55cd210481fdf467fd6ec1aa42fc38
De-natural <festuschidubem376@gmail.com>
2025-12-08 18:41:01 +0100

    small changes

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -57,13 +57,13 @@ const Footer = () => {
       <div className="p-4 flex gap-y-2 flex-col lg:flex-row lg:justify-around lg:items-center">
         <p className="text-xs font-normal lg:text-lg">&copy; { year } easedrive. All rights reserved.</p>
         
-        <a href="/terms-of-use">
-          <p className="text-xs font-normal lg:text-lg">Terms of use</p>
-        </a>
+         <Link to={"/terms-of-use"}>
+         <p className="text-xs font-normal lg:text-lg">Terms of use</p>
+         </Link>
         
-        <a href="/privacy">
-          <p className="text-xs font-normal lg:text-lg">Privacy Policy</p>
-        </a>
+        <Link to={"/privacy"}>
+        <p className="text-xs font-normal lg:text-lg">Privacy Policy</p>
+        </Link>
         
         <p className="text-xs font-normal lg:text-lg">Proudly built by students, for students.</p>
     </div>
